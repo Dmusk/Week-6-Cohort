83fbed7b0c7bdb1465fcb09c4d9e987be17c69f5
Dmusk <aadarsh.wadile@mitaoe.ac.in>
2024-07-12 23:03:42 +0530

    undertand the Key using Todo App

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,54 @@
 import { useState } from "react";
 
-
+var i = 3;
 function App() {
+  const [todos, setTodos] = useState([{
+    id: 1,
+    Title: "Gym",
+    Desc: "6-7",
+    Completed: "false",
+  },
+  {
+    id: 2,
+    Title: "Study",
+    Desc: "9-12",
+    Completed: "false",
+  },
+  {
+    id: 3,
+    Title: "Sleep",
+    Desc: "12-7",
+    Completed: "false",
+  }])
 
-  return (
-    <div>
-      <Firsthandler></Firsthandler>
-      <Header title="I am from Dondaicha"></Header>
-    </div>
-  )
-}
-
-function Firsthandler() {
-
-  const [first, setFirst] = useState("My name is Aadarsh");
-
-  function onClickHandler() {
-    setFirst("My name is " + Math.random());
+  function addTodo() {
+    i++;
+    setTodos([...todos,        //... -> spread operator;
+    {
+      id: i,
+      Title: "BreakFast",
+      Desc: "8-9",
+      Completed: "false",
+    }]
+    )
   }
 
   return (
     <div>
-      <button onClick={onClickHandler}>Change title</button> <br />
-      <Header title={first}></Header>
-      <br />
+      <button onClick={addTodo}>Add Todo</button>
+      {todos.map(todo => <Todo key={todo.id} title={todo.Title} desc={todo.Desc} stat={todo.Completed} />)}
     </div>
   )
 }
 
-function Header({ title }) {
+function Todo({ title, desc, stat }) {
   return <>
-    {title}
+    <div>
+      <h3>{title}</h3>
+      <h3>{desc}</h3>
+      <h3>{stat}</h3>
+      <hr />
+    </div>
   </>
 }
 
